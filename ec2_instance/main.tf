816769eb7fa3501c18d9a714e2c013a58dab7d59
sonyhl05 <sonyhl1918@gmail.com>
2025-03-06 16:49:32 +0530

    Create main.tf

---
+++ ec2_instance/main.tf
@@ -0,0 +1,57 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = var.cidr_block
+
+  tags = {
+    Name = "${var.name}-vpc"
+  }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet(var.cidr_block, 8, 1) 
+  availability_zone = var.availability_zone
+
+  tags = {
+    Name = "${var.name}-subnet"
+  }
+}
+
+resource "aws_security_group" "allow_ssh_http" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks  = [var.cidr_block]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks  = [var.cidr_block]
+  }
+
+  tags = {
+    Name = "${var.name}-sg"
+  }
+}
+
+resource "aws_instance" "app" {
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.main.id
+  vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
+
+  tags = {
+    Name = "${var.name}-instance"
+  }
+}
+
+can you try this?
+with adding public subnet resource
