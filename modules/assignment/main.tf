3711dfbf1d8b21370be7fa52572e6b7e60ab892e
sonyhl05 <sonyhl1918@gmail.com>
2025-03-09 23:26:40 +0530

    Update main.tf

--- modules/assignment/main.tf
+++ modules/assignment/main.tf
@@ -5,6 +5,20 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-00c257e12d6828491" // Replace with the AMI ID you want to use
   instance_type = "t2.micro"
+  user_data = <<-EOF
+              #!/bin/bash
+              # Update package list and install prerequisites
+              apt-get update -y
+              apt-get install -y software-properties-common
+
+              # Add Ansible PPA and install Ansible
+              apt-add-repository --yes --update ppa:ansible/ansible
+              apt-get install -y ansible
+
+              # Optionally, you could log the installation status
+              echo "Ansible installed successfully" > /tmp/ansible_install.log
+              EOF
+
 
   tags = {
     Name = "my_ec2_instance"
