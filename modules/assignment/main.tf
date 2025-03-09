a721171b6bca6c2bd00110a1b1ef788c28e7736f
sonyhl05 <sonyhl1918@gmail.com>
2025-03-09 23:22:31 +0530

    Create main.tf

---
+++ modules/assignment/main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region = "us-west-2" // Replace with your desired AWS region
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-00c257e12d6828491" // Replace with the AMI ID you want to use
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "my_ec2_instance"
+  }
+
+  provisioner "local-exec" {
+    command = "echo ${self.public_ip} > ip_address.txt"
+  }
+}
+
+output "instance_ip_addr" {
+  value = aws_instance.example.public_ip
+}
